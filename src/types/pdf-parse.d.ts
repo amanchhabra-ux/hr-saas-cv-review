34f96d19be9e78738815f85ee89b6440a58f6d00
amanchhabra-ux <aman.chhabra@gmail.com>
2026-06-28 14:58:23 +0530

    Downgrade pdf-parse to v1.1.1 to resolve native C++ addon canvas dependency issue on Vercel

---
+++ src/types/pdf-parse.d.ts
@@ -0,0 +1,12 @@
+declare module 'pdf-parse' {
+  interface PDFParseData {
+    numpages: number;
+    numrender: number;
+    info: any;
+    metadata: any;
+    version: string;
+    text: string;
+  }
+  function pdfParse(dataBuffer: Buffer, options?: any): Promise<PDFParseData>;
+  export default pdfParse;
+}
